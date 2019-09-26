506c4de5f08135e6b3b9b0675c826b9766e73e5e
chengkai <cheng_kai@suixingpay.com>
2019-09-26 21:01:22 +0800

    redux 请求

---
+++ app/config/url.conf.js
@@ -0,0 +1,5 @@
+/**
+ * Create by chengkai on 2019/9/26.
+ * Describe:
+ */
+export const ACCOUNT_STATUS = 'ACCOUNT_STATUS';
---
+++ app/net-request/index.js
@@ -0,0 +1,21 @@
+/**
+ * Create by chengkai on 2019/7/29.
+ * Describe:
+ */
+
+import {asyncRequestBase, METHOD} from './base';
+import {ACCOUNT_STATUS} from '../config/url.conf';
+
+/**
+ * import { fetchAccountStatus } from "../../net-request";
+ * @connect(state => ({
+ *   accountStatus: state.promiseReducer.accountStatus,
+ * }))
+ */
+export const fetchAccountStatus = fetchInstance =>
+  asyncRequestBase({
+    method: METHOD.POST,
+    url: ACCOUNT_STATUS,
+    fetchInstance,
+    key: 'accountStatus',
+  });
